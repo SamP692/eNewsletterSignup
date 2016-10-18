41d388dea4ead5dc138434f90c642e292ac0a62a
Sam Peterson <samuel.e.peterson@gmail.com>
2016-10-18 19:49:17 -0400

    initialize repository

---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const app = require('./app/app');
